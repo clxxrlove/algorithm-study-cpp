008087e82a95ebfd6138ae4acb762f137a66e071
Jiyong <hickerd@sch.ac.kr>
2024-12-30 10:25:41 +0900

    Initial Commit

---
+++ algorithm-study-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm-study-cpp
+//
+//  Created by 정지용 on 12/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
