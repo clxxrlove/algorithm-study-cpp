4e79c3255860b7663c478167c4bce53e4f19722a
Jiyong <hickerd@sch.ac.kr>
2025-01-20 10:12:15 +0900

    [Add] 2025-01-20 / 1922번 문제 풀이

---
+++ 1922/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1922
+//
+//  Created by 정지용 on 1/20/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
