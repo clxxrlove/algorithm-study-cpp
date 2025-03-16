e5427253b63c0f567f34aefbbfbefc181bab8ad2
Jiyong <hickerd@sch.ac.kr>
2025-03-17 06:00:05 +0900

    [Add] 2025-03-17 / 27323번 문제 풀이

---
+++ 27323/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  27323
+//
+//  Created by 정지용 on 3/17/25.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b; cin >> a>> b;
+    cout << a * b;
+    return 0;
+}
